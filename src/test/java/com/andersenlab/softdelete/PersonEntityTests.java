9adeeffa5f8aa459337104c06a79bcd174986941
Sergey Nemets <s.nemets@andersenlab.com>
2022-02-10 17:30:03 +0300

    add lazy initialization to illustrate LazyInitializationException

--- src/test/java/com/andersenlab/softdelete/PersonEntityTests.java
+++ src/test/java/com/andersenlab/softdelete/PersonEntityTests.java
@@ -2,15 +2,17 @@ package com.andersenlab.softdelete;
 
 import com.andersenlab.softdelete.repository.ActionRepository;
 import com.andersenlab.softdelete.repository.PersonRepository;
+import org.hibernate.LazyInitializationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.jpa.properties.hibernate.enable_lazy_load_no_trans=true")
 @Sql(statements = {"TRUNCATE TABLE person CASCADE", "insert into person values (1, 'John', false)", "insert into action values (1, 1, 'Greeting')" })
 class PersonEntityTests {
 
@@ -38,9 +40,20 @@ class PersonEntityTests {
         assertTrue(actionRepository.findByIdEager(1L).get().getPerson().isDeleted());   //However, dependent entities could fetch the entity marked as deleted with eager...
 
         assertTrue(actionRepository.findById(1L).isPresent());
-        assertTrue(actionRepository.findById(1L).get().getPerson().isDeleted());        //... or lazy fetch
+        assertTrue(actionRepository.findById(1L).get().getPerson().isDeleted());        //... or lazy fetch (only in transaction, see below)
 
         assertEquals(0L, personRepository.findAllDeleted2().size());            //The ugliest query condition : where ( person0_.is_deleted = false) and person0_.is_deleted=true
+    }
+
+    @Test
+    void testLazyLoad() {
+        personRepository.deleteById(1L);
+
+        // one more thing.Outside the transaction and with spring.jpa.properties.hibernate.enable_lazy_load_no_trans=true property
+        // on lazy fetch you will get the exception because the @Where condition is applied
+        assertTrue(actionRepository.findById(1L).isPresent());
+        final var action =  actionRepository.findById(1L).get();
+        assertThrows(LazyInitializationException.class, () -> System.out.println(action.getPerson()));
     }
 
 }
